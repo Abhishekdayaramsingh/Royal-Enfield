933b71af5bdabad5a06ab2660b843808d56a2791
Abhishekdayaramsingh <137001482+Abhishekdayaramsingh@users.noreply.github.com>
2023-10-23 14:44:23 +0530

    Add files via upload

---
+++ re.js
@@ -0,0 +1,112 @@
+function a(){
+    x = document.querySelector("#j81")
+    x.src="./img/81.jpg"
+}
+function b(){
+    x = document.querySelector("#j81")
+    x.src="./img/82.jpg"
+}
+function c(){
+    x = document.querySelector("#j81")
+    x.src="./img/83.jpg"
+}
+function d(){
+    x = document.querySelector("#j81")
+    x.src="./img/84.jpg"
+}
+function e(){
+    x = document.querySelector("#j81")
+    x.src="./img/85.jpg"
+}
+function f(){
+    x = document.querySelector("#j81")
+    x.src="./img/81.jpg"
+}
+function g(){
+    x = document.querySelector("#j81")
+    x.src="./img/87.jpg"
+}
+function h(){
+    x = document.querySelector("#j81")
+    x.src="./img/82.jpg"
+}
+function a1(){
+    x = document.querySelector("#j82")
+    x.src="./img/81.jpg"
+}
+function b1(){
+    x = document.querySelector("#j82")
+    x.src="./img/82.jpg"
+}
+function c1(){
+    x = document.querySelector("#j82")
+    x.src="./img/83.jpg"
+}
+function d1(){
+    x = document.querySelector("#j82")
+    x.src="./img/84.jpg"
+}
+function e1(){
+    x = document.querySelector("#j82")
+    x.src="./img/85.jpg"
+}
+function f1(){
+    x = document.querySelector("#j82")
+    x.src="./img/81.jpg"
+}
+function g1(){
+    x = document.querySelector("#j82")
+    x.src="./img/87.jpg"
+}
+function h1(){
+    x = document.querySelector("#j82")
+    x.src="./img/82.jpg"
+}
+function a2(){
+    x = document.querySelector("#j83")
+    x.src="./img/81.jpg"
+}
+function b2(){
+    x = document.querySelector("#j83")
+    x.src="./img/82.jpg"
+}
+function c2(){
+    x = document.querySelector("#j83")
+    x.src="./img/83.jpg"
+}
+function d2(){
+    x = document.querySelector("#j83")
+    x.src="./img/84.jpg"
+}
+function e2(){
+    x = document.querySelector("#j83")
+    x.src="./img/85.jpg"
+}
+function f2(){
+    x = document.querySelector("#j83")
+    x.src="./img/81.jpg"
+}
+function g3(){
+    x = document.querySelector("#j84")
+    x.src="./img/87.jpg"
+}
+function h3(){
+    x = document.querySelector("#j84")
+    x.src="./img/82.jpg"
+}
+function a3(){
+    x = document.querySelector("#j85")
+    x.src="./img/81.jpg"
+}
+function b3(){
+    x = document.querySelector("#j85")
+    x.src="./img/82.jpg"
+}
+function c3(){
+    x = document.querySelector("#j85")
+    x.src="./img/83.jpg"
+}
+function x(){
+    x =document.querySelector("#j81")
+    x.src="./img/88.jpg"
+}
